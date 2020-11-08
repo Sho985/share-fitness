647ca079d130da2986468af611a7a904cd06952d
Sho985 <bij97kwsks@gmail.com>
2020-11-08 19:26:06 +0900

    Add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  validates :name, presence: true
+  validates :self_introduction, presence: true
+  validates :image, presence: true
+  
   #carriewave・Userモデルとの紐付け
   mount_uploader :image,  UserimageUploader
 end
