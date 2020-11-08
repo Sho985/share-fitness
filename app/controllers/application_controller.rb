6b95dde901e1038a89e967f349e28a37080e654d
Sho985 <bij97kwsks@gmail.com>
2020-11-08 16:41:32 +0900

    Create sign-up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protected
+  #deviseにname,self_introduction,imageカラムを追加
+  def configure_permitted_parameters
+    added_attrs = [ :email, :name, :self_introduction,:password, :password_confirmation, :image ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
 end
