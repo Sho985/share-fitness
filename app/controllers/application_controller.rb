2a8a5d6f8a8db652d38f6e598dfb9a62d16d8eb7
Sho985 <bij97kwsks@gmail.com>
2020-11-08 21:04:12 +0900

    Add devise settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  
+  #追加したカラムをdeviseに反映するために追記
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   #deviseにname,self_introduction,imageカラムを追加
   def configure_permitted_parameters
