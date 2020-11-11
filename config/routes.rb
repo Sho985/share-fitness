5548f8fabe9874830974400c64edadf001de61bd
Sho985 <bij97kwsks@gmail.com>
2020-11-11 20:59:50 +0900

    Add posting function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   root 'static_pages#home'
   
   #ユーザーマイページ用のルーティング
   resources :users, :only => [:show]
+
+  resources :posts
 end
