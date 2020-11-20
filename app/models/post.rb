577b10551e3e55521653f29ed657362b5919be88
Sho985 <bij97kwsks@gmail.com>
2020-11-20 15:13:14 +0900

    Add postimage uploader

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,7 @@ class Post < ApplicationRecord
   #training_menuモデルとの関連付け/training_menuモデルをPostモデルにネストさせる。
   has_many :training_menus, dependent: :destroy
   accepts_nested_attributes_for :training_menus, reject_if: :all_blank, allow_destroy: true
+
+  #carriewave・Postモデルとの紐付け
+  mount_uploader :image,  PostimageUploader
 end
