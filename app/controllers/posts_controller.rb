fd1a7fd77c8f6a1c8619b73a205ad7d4e1a499aa
Sho985 <bij97kwsks@gmail.com>
2020-11-25 22:08:59 +0900

    Add post delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
+  #投稿者以外、編集・削除できない
+  before_action :correct_user, only: [:destroy]
+
   def index
     @posts = Post.all.includes(:training_menus)
   end
@@ -19,11 +23,24 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path, notice: '削除が完了しました！'
+  end
 
     private
 
     def post_params
       params.require(:post).permit(:comment, :image, 
       training_menus_attributes: %i[id part event weight repetition set_count]).merge(user_id: current_user.id)
+    end
+
+    #投稿ユーザーとログインユーザーが正しくないとアクセスできない
+    def correct_user
+      post = Post.find(params[:id])
+      if post.user_id != current_user.id
+        redirect_to user_path(current_user), alert: 'アクセス権限がありません'
+      end
     end
 end
