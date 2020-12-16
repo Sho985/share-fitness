7f52188fa1a94a15c656130be034afb200307e8a
Sho985 <bij97kwsks@gmail.com>
2020-12-16 20:07:42 +0900

    Added edit / delete function to question function

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,8 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @questions = Question.all
+    @q = Question.all.ransack(params[:q])
+    @questions = @q.result(distinct: true)
   end
 
   def new
@@ -16,6 +17,25 @@ class QuestionsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to questions_path, notice: '更新が完了しました！'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    question = Question.find(params[:id])
+    question.destroy
+    redirect_to questions_path, notice: '削除が完了しました！'
   end
 
   private
