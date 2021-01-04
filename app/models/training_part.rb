bbb6994314ae0eab3a05621e1d7a44c919536214
Sho985 <bij97kwsks@gmail.com>
2021-01-04 15:38:01 +0900

    Change background image of post page & add validation to post function

--- app/models/training_part.rb
+++ app/models/training_part.rb
@@ -1,5 +1,6 @@
 class TrainingPart < ApplicationRecord
-  validates :part, presence: true
+  validates :part, presence: true, uniqueness: { scope: :post,
+  message: "が重複しています。" }
   belongs_to :user
   belongs_to :post
 end
