4daaf5f303d16c9d8bc026316493a31c9153dc4c
Sho985 <bij97kwsks@gmail.com>
2021-01-04 17:17:19 +0900

    Add guest user login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {   registrations: 'users/registrations',
-  sessions: 'users/sessions' }              
   root 'static_pages#home'
+
+  devise_for :users, controllers: {   registrations: 'users/registrations',
+  sessions: 'users/sessions' }
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   
   resources :users, only: [:show] do
     get :calendar, on: :member 
